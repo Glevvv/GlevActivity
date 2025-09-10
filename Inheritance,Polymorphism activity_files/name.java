136f60de51a4c9584c0b50b18d6b3dfe9e2db7e6
Glevvv <146816404+Glevvv@users.noreply.github.com>
2025-09-10 13:19:48 +0800

    Create name.java

---
+++ Inheritance,Polymorphism activity_files/name.java
@@ -0,0 +1,18 @@
+
+class name {
+
+    public static void main(String[] args) {
+        String[] ALPHABET = {
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ",
+            "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+        };
+
+
+    // Write your Full Name using the array
+    String firstName =  // Complete Here
+    String lastName =  // Complete Here
+    System.out.println("Full Name: " + firstName + " " + lastName);
+
+    }
+
+}
